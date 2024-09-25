4e8bd69e373292cef6c59909439aca3ee5587b8b
robiul99siyam <siyamrobiulislam@gmail.com>
2024-09-26 00:18:51 +0600

    react and tailwind css setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  
+  return (
+    <>
+      <div >
+        <h1 className="text-3xl font-bold underline text-green-500">hello world</h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
